a6c97eaf4c091be214ea324666a6bf90532db550
abiyasa <ilham.suhardi@gmail.com>
2012-05-09 11:49:11 +0200

    add node file to test it online

---
+++ testNode.js
@@ -0,0 +1,23 @@
+
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var app = module.exports = express.createServer();
+
+// Configuration
+app.configure(function(){
+  app.use(express.static(__dirname + '/.'));  
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// starts yo!
+app.listen(3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
